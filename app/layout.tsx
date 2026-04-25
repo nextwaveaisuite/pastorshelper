3e098a82e2d80bcafcc86564f7794373c272a3eb
nextwaveaisuite <nextwaveaisuite@gmail.com>
2026-04-25 20:33:46 +1000

    Update Google verification code in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,7 +93,7 @@ export const metadata: Metadata = {
     canonical: "https://thepastorshelper.com",
   },
   verification: {
-    google: "add-your-google-search-console-verification-code-here",
+    google: "VLL5j4c-dDjxxAJGOIIjqzbOIGyp2d1l-RZVsfTaQaY",
   },
   icons: {
     icon: "/favicon.ico",
